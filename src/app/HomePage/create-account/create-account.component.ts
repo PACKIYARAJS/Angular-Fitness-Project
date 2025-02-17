c84238b94c17e6e31a051b8a24bf8051498f57ba
PACKIYARAJS <packiyarajs350@gmail.com>
2025-02-17 16:02:22 +0530

    Updates

--- src/app/HomePage/create-account/create-account.component.ts
+++ src/app/HomePage/create-account/create-account.component.ts
@@ -1,47 +1,76 @@
-import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
-import { create } from 'domain';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Router, RouterModule } from '@angular/router';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { ApiService } from '../../Services/api.service';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [FormsModule, RouterModule, RouterOutlet],
+  imports: [FormsModule, ReactiveFormsModule, RouterModule, CommonModule, NgbAlertModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss'
 })
 export class CreateAccountComponent {
-    FirstName:String = '';
-    LastName:String = '';
-    UserName:String = '';
-    Age:Number = 0;
-    Gender = '';
-    Email = '';
-    ContactNumber:Number = 0;
-    Password:String = '';
 
-    constructor(private api:ApiService,private router:Router){}
+    registerForm : FormGroup;
+    isSubmitted : boolean = false;
+    isRegisterSuccess : boolean = false; 
+
+    constructor(private api:ApiService,private router:Router)
+    {
+      this.registerForm = new FormGroup
+      (
+        {
+          FirstName: new FormControl('', Validators.required),
+          LastName: new FormControl('', Validators.required),
+          UserName: new FormControl('', Validators.required),
+          Age: new FormControl(0),
+          Gender: new FormControl('', Validators.required),
+          Email: new FormControl('', [Validators.email]),
+          ContactNumber: new FormControl(0, [Validators.minLength(10), Validators.required]),
+          Password: new FormControl('', [Validators.minLength(8), Validators.required]),
+          ConfirmPassword: new FormControl('', [Validators.minLength(8), Validators.required])
+        }
+      );
+    }
+
+    validPassword()
+    {
+      if(this.registerForm.get('Password')?.value != this.registerForm.get('ConfirmPassword')?.value)
+      {
+        alert('Password and Confirm Password Doesnt match...');
+        return false;
+      }
+      return true;
+    }
 
     CreateData(){
 
-      let request={
-        FirstName : this.FirstName,
-        LastName : this.LastName,
-        UserName : this.UserName,
-        Age : this.Age,
-        Gender : this.Gender, 
-        Email : this.Email,
-        ContactNumber : this.ContactNumber,
-        Password : this.Password
+      this.isSubmitted = true;
+
+      if(this.validPassword() && this.registerForm.status=='VALID')
+      {
+        let request={
+          FirstName : this.registerForm.get('FirstName')?.value,
+          LastName : this.registerForm.get('LastName')?.value,
+          UserName : this.registerForm.get('UserName')?.value,
+          Age : this.registerForm.get('Age')?.value,
+          Gender : this.registerForm.get('Gender')?.value, 
+          Email : this.registerForm.get('Email')?.value,
+          ContactNumber : this.registerForm.get('ContactNumber')?.value,
+          Password : this.registerForm.get('Password')?.value,
+          ConfirmPassword : this.registerForm.get('ConfirmPassword')?.value,
+          UserType:'Customer'
+        }
+        this.api.create(request).subscribe(
+          ()=>{
+            alert('Registration successfully....')
+            this.router.navigate(['/login']);
+          },
+          err=>{console.log(err)}
+        );
       }
-      this.api.create(request).subscribe(
-        ()=>{
-          alert('Registration successfully....')
-          this.router.navigate(['/login']);
-        },
-        err=>{console.log(err)}
-      );
     }
 }
