962d6e8ad5c04ad020e08023632049b533d4f50d
PACKIYARAJS <packiyarajs350@gmail.com>
2025-03-03 11:00:34 +0530

    Contact us

--- src/app/HomePage/contactus/contactus.component.ts
+++ src/app/HomePage/contactus/contactus.component.ts
@@ -1,12 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contactus.component.html',
   styleUrl: './contactus.component.scss'
 })
 export class ContactusComponent {
+
+  image = 'assets/images4.jpg';
+
+  ContactUS : FormGroup;
+
+  isSubmitted : boolean = false;
+
+  constructor()
+  {
+    this.ContactUS = new FormGroup(
+      {
+        email : new FormControl('', [Validators.email, Validators.required]),
+
+        phoneNumber : new FormControl('', [Validators.minLength(10), Validators.maxLength(10), Validators.required]),
+
+        text : new FormControl('', Validators.required)
+      }
+    );
+  }
+
+  GoToSubmit()
+{
+  this.isSubmitted = true;
+
+  console.log(this.ContactUS);
+
+  if(this.ContactUS.status == 'VALID')
+  {
+    let request = {
+
+      email : this.ContactUS.get('email')?.value,
+
+      phoneNumber : this.ContactUS.get('phoneNumber')?.value,
+
+      Massage : this.ContactUS.get('text')?.value
+    }
+    alert('Message sended successfully....');
+
+  }
+}
 
 }
