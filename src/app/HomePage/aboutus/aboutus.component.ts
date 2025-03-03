4e067fa0830c0175526b427efa8df5cc741fd216
PACKIYARAJS <packiyarajs350@gmail.com>
2025-03-03 09:16:10 +0530

    About us

--- src/app/HomePage/aboutus/aboutus.component.ts
+++ src/app/HomePage/aboutus/aboutus.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbRatingConfig, NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-aboutus',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgbRatingModule],
   templateUrl: './aboutus.component.html',
   styleUrl: './aboutus.component.scss'
 })
 export class AboutusComponent {
+
+  constructor(rating : NgbRatingConfig) 
+  {
+      rating.max = 5;
+
+      rating.readonly = true;
+  }
 
 }
